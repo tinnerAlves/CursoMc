2e53b7a30565c422ab05d87eb7dc23770cfd3978
Tinner <guifelan158@gmail.com>
2026-02-25 20:11:24 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/br/com/cursoMc/projetoOrientado/domain/Categoria.java
+++ src/main/java/br/com/cursoMc/projetoOrientado/domain/Categoria.java
@@ -2,9 +2,17 @@ package br.com.cursoMc.projetoOrientado.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     
