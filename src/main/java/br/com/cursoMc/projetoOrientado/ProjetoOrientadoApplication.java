2ccf00d51b60978b43c091d0f857c39af8bd6516
Tinner <guifelan158@gmail.com>
2026-02-25 19:19:07 -0300

    Começo

---
+++ src/main/java/br/com/cursoMc/projetoOrientado/ProjetoOrientadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursoMc.projetoOrientado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOrientadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOrientadoApplication.class, args);
+	}
+
+}
