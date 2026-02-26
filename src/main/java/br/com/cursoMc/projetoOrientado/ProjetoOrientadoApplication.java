35b68377c466bc67dcc0ed6f02ec5034ba5b0f7b
Tinner <guifelan158@gmail.com>
2026-02-26 15:15:34 -0300

    Instancia criando

--- src/main/java/br/com/cursoMc/projetoOrientado/ProjetoOrientadoApplication.java
+++ src/main/java/br/com/cursoMc/projetoOrientado/ProjetoOrientadoApplication.java
@@ -1,13 +1,32 @@
 package br.com.cursoMc.projetoOrientado;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.cursoMc.projetoOrientado.domain.Categoria;
+import br.com.cursoMc.projetoOrientado.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetoOrientadoApplication {
+public class ProjetoOrientadoApplication  implements CommandLineRunner {
+
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoOrientadoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
